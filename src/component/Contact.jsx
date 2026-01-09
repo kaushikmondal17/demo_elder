eae12c8f65f8c2f93f57c07f097ab4ae219fbc43
Kaushik Mondal <167294922+kaushikmondal17@users.noreply.github.com>
2026-01-09 17:26:50 +0530

    footer

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -3,7 +3,105 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      
+        {/* banner */}
+  <div className="inner_banner animate-on-scroll">
+  <img src="images/about_banner.webp" />
+  <div className="container">
+    <div className="row">
+      <div className="col-md-12">
+        <div className="heading_part">
+          <h2 className="animate-on-scroll">Contact Us</h2>
+          <p><a href="index.html">Home</a> / Contact Us</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+{/* <!-- contact_box --> */}
+
+<div className="contact_part animate-on-scroll">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-4">
+        <div className="contact_box animate-on-scroll wow fadeInRight">
+          <i className="fa-solid fa-envelope-circle-check" />
+          <h4 className="animate-on-scroll">Email Address</h4>
+          <p>info@webmail.com</p>
+          <p>info@webmail.com</p>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="contact_box animate-on-scroll wow fadeInRight">
+          <i className="fa-solid fa-phone-volume" />
+          <h4 className="animate-on-scroll">Phone Number</h4>
+          <p>+0123-456789</p>
+          <p>+0123-456789</p>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="contact_box animate-on-scroll wow fadeInRight">
+          <i className="fa-solid fa-location-dot" />
+          <h4 className="animate-on-scroll">Office Address</h4>
+          <p>18/A, New Born Town Hall</p>
+          <p>New York, US</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+{/* <!-- contact_form --> */}
+
+<div className="contact_form animate-on-scroll">
+  <div className="container">
+    <div className="row">
+      <div className="main_container_box">
+        <h4 className="animate-on-scroll">Get a Queue</h4>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="text_form">
+              <input type="text" placeholder="Enter Your Name" />
+              <i className="fa-solid fa-user" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="text_form">
+              <input type="mail" placeholder="Enter Your Email Address" />
+              <i className="fa-solid fa-envelope" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="text_form">
+              <select className="form-select" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value={1}>One</option>
+                <option value={2}>Two</option>
+                <option value={3}>Three</option>
+              </select>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="text_form">
+              <input type="text" placeholder="Enter Your Phone Number" />
+              <i className="fa-solid fa-phone-volume" />
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="text_form">
+              <textarea placeholder="Enter Your Msg" className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+              <i className="fa-solid fa-pen" />
+            </div>
+          </div>
+          <button>get A Free Service</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     </div>
   )
 }
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,67 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <p>footer</p>
+<footer className="footer animate-on-scroll">
+  <div className="container">
+    <div className="row">
+      <div className="col-md-3">
+        <div className="logo_footer">
+          <img src="images/Logo.png" />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed distinctio quae quas placeat
+            aspernatur perspiciatis non modi at quibusdam doloribus.</p>
+          <div className="footer_social_icon">
+            <i className="fa-brands fa-whatsapp" />
+            <i className="fa-brands fa-facebook-f" />
+            <i className="fa-brands fa-youtube" />
+            <i className="fa-brands fa-twitter" />
+          </div>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="information_footer">
+          <h4>information</h4>
+          <ul>
+            <li><a href="#">New Product</a></li>
+            <li><a href="#">Top Seller</a></li>
+            <li><a href="#">Ore Blog</a></li>
+            <li><a href="#">Ore Blog</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="contact_footer">
+          <h4>Contact Us</h4>
+          <p> <i className="fa-solid fa-location-dot" />123 Main Street, City, Country</p>
+          <p><i className="fa-solid fa-phone" /> +1234567890</p>
+          <p><i className="fa-solid fa-envelope-circle-check" /> info@eldepharma.com</p>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="footer_newsletter">
+          <h4>Newsletter</h4>
+          <p>Subscribe to our weekly Newsletter and receive updates via email.</p>
+          <div className="form">
+            <input type="email" placeholder="Enter your email" />
+            <button>Subscribe</button>
+          </div>
+          <div className="pay_img_section">
+            <h5>We Accept</h5>
+            <div className="pay_img">
+              <img src="images/visa.png" alt="Mastercard" />
+              <img src="images/Mastercard-PNG-Photos.png" alt="Visa" />
+              <img src="images/visa.png" alt="Mastercard" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="bottom_footer">
+    <p>© 2024 Eldepharma.</p>
+    <p> All rights reserved.</p>
+  </div></footer>
+
+
     </div>
   )
 }
